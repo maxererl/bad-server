a62e9fd905fbf3c2bd4d51422448c124efe4a812
maxererl <maxererl@yandex.ru>
2026-04-26 18:06:41 +0300

    Updated dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,35 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ svgr(), react(), tsconfigPaths({root: __dirname})],
+  plugins: [
+    react(),
+    svgr(),
+  ],
+
   resolve: {
     alias: {
-      $fonts: resolve('./src/vendor/fonts'),
-      $assets: resolve('./src/assets'),
-    }
+      $fonts: resolve(__dirname, 'src/vendor/fonts'),
+      $assets: resolve(__dirname, 'src/assets'),
+    },
+
+    tsconfigPaths: true,
   },
+
   build: {
-    assetsInlineLimit:0,
+    assetsInlineLimit: 0,
   },
+
   css: {
     preprocessorOptions: {
       scss: {
         additionalData: `
-          @use "./src/scss/variables" as *;
-          @use "./src/scss/mixins";
+          @use "/src/scss/variables" as *;
+          @use "/src/scss/mixins" as mixins;
         `,
-      },
-
-    }
+      }
+    },
   },
-
-})
+});
