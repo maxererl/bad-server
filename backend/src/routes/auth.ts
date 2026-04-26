4233d9975dbef6751de4f62427e29d66ad5f64f2
maxererl <maxererl@yandex.ru>
2026-04-26 15:57:57 +0300

    CSRF protection added

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,3 +1,4 @@
+import csurf from 'csurf'
 import { Router } from 'express'
 import {
     getCurrentUser,
@@ -12,12 +13,18 @@ import auth from '../middlewares/auth'
 
 const authRouter = Router()
 
+const csrfProtection = csurf({ cookie: true });
+
+authRouter.get('/csrf-token', csrfProtection, (req, res) => {
+    res.send({ csrfToken: req.csrfToken() });
+});
+
 authRouter.get('/user', auth, getCurrentUser)
-authRouter.patch('/me', auth, updateCurrentUser)
+authRouter.patch('/me', auth, csrfProtection, updateCurrentUser)
 authRouter.get('/user/roles', auth, getCurrentUserRoles)
-authRouter.post('/login', login)
+authRouter.post('/login', csrfProtection, login)
 authRouter.get('/token', refreshAccessToken)
 authRouter.get('/logout', logout)
-authRouter.post('/register', register)
+authRouter.post('/register', csrfProtection, register)
 
 export default authRouter
